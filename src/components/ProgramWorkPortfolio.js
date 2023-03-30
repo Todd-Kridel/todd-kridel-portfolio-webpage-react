f3fa92789eadd5cb7dc6ea6df809f2b9f64339be
Todd Kridel <todd.kridel@gmail.com>
2023-03-29 19:22:33 -0600

    README refinements

--- src/components/ProgramWorkPortfolio.js
+++ src/components/ProgramWorkPortfolio.js
@@ -6,7 +6,7 @@ import ProgramWorkProjectsList from "./ProgramWorkProjectsList.js";
 
 
 // WHEN I am presented with the Portfolio section...
-// THEN I see titled images of six of the developer’s applications with links to both the deployed 
+// THEN I see titled images of six+ of the developer’s applications with links to both the deployed 
 // applications and the corresponding GitHub repository.
 
 
